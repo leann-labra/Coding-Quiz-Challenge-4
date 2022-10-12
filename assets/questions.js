8eebfb1d4db2793d417c5ea075b3eaa016b77585
leann-labra <leann.labra@gmail.com>
2022-10-12 05:05:37 -0700

    got stuck :( and then i remembered i could make all these DOMs through js :( silly goose

---
+++ assets/questions.js
@@ -0,0 +1,13 @@
+// array of questions to draw from 
+var questions= [
+{ 
+    q: 'The ___ tag provides information to the browser about your webpage including the author name and keywords', 
+    a:'4. <meta></meta>',
+    choices:[{choice: "1. <html></html>"}, {choice: "2. <body></body>"}, {choice:"3. <style>/<style> "}, {choice:'4. <meta></meta>'}]
+}
+{ 
+    q: "What does CSS stand for?",
+    a: '4. Cascading Style Sheet',
+    choices:[{choice:"1. Custom Style Sheets"}, {choice:"2. Cool Style Sheet"}, {choice:"3. Coding Style Sheet"}, {choice:"4. Cascading Style Sheet"}]
+}
+]
