5b6610577de66058ad66af0b0100913d338b3840
leann-labra <leann.labra@gmail.com>
2022-10-12 12:18:13 -0700

    finally got questions to show up after initial start quiz click.... erm

--- assets/game.js
+++ assets/game.js
@@ -1,3 +1,34 @@
+//declared questions array
+var questions= [
+    {
+        title: "In JavaScript, what element is used to store multiple values in a single variable?",
+        choices: ["strings", "array", "list", "function"],
+        answer: "arrays"
+    },
+    {
+        title: "What is considered to be the most popular programming language in the world?",
+        choices: ["JavaScript", "Swift", "HTML", "Python"],
+        answer: "Javascript"
+    },
+    {
+        title: "What kind of statement is used to execute actions based on a trigger or condition?",
+        choices: ["regular expression", "boolean", "conditional statement", "all of the above"],
+        answer: "conditional statement"
+    },
+    {
+        title: "Which of the following includes Chrome's V8 JavaScript Engine?",
+        choices: ["jQuery", "Java", "npm", "Node.js"],
+        answer: "Node.js"
+    },
+    {
+        title: "What is the correct syntax for referring to an external script called xxx.js?",
+        choices: ["<script src=xxx.js>", "<script name='xxx.js'>", "<script href='xxx.js'>", "<script src='xxx.js'>"],
+        answer: "<script src='xxx.js'>"
+    }
+
+];
+
+
 //declared variables for game
 var questionsIndex = 0;
 var score = 0;
@@ -14,12 +45,12 @@ var choiceList = document.querySelector("#choices");
 var quizContent = document.querySelector("#quiz-content");
 var WoR = document.querySelector("#WoR");
 
-var highscoreReveal = document.querySelector("#highscore");
+var scoreReveal = document.querySelector("#score");
 
 //timer 
 startBtn.addEventListener("click", function timerCountdown() {
 
-    var timerInterval = setInterval(function() {
+    var timerInterval = setInterval( function() {
         secondsLeft--;
         timer.textContent = secondsLeft;
     
@@ -29,15 +60,16 @@ startBtn.addEventListener("click", function timerCountdown() {
         alert("ran out of time sorry :(");};
 
         }, 1000);//with setInterval function
-        
+    
     document.getElementById("titlePage").style.display="none";
-   
-    showQuestions();//not defined? why
-     },
-   
+    console.log(document);
+    
+    showQuestions(); 
+
+    },
   );
 
-function showQuestions() {
+ function showQuestions() {
 
    //reveal question taken from question index
    for (var i=0; i <questions.length; i++) {
@@ -45,50 +77,63 @@ function showQuestions() {
    var questionShown = questions[questionsIndex].title;
    var questionChoices =questions[questionsIndex].choices;
    question.textContent = questionShown;
-   choices.textContent = questionChoices;
+   choiceList.textContent = questionChoices;
+   
+    //checking to see if Array can be recalled from global object
+        //  console.log(questionShown);
+        //  console.log(questionChoices);
 
-//    console.log(questionShown);
-//    console.log(questionChoices);
-
-    question.choices.forEach(function(choice, i) {
     // creates a new button for each choice
-    var choiceBtn = document.createElement("button");
-    choiceBtn.setAttribute("class", "choice");
-    choiceBtn.setAttribute("value", choice);
-
-    choiceBtn.textContent = i + 1 + ". " + choice;
-
-    // attach click event listener to each choice
-    choiceBtn.onclick = clickedQuestion;
-
-    // display on the page
-    choiceList.appendChild(choiceBtn);
-  });
-   }
-} 
-
-
-//now, I need to make a function for the clicked question to determine whether or not it is correct
-var clickedQuestion = function clickedQuestion() {
-
-if(this.value !== questions[questionsIndex].answer) {
-    //penalty suffered
-    secondsLeft= secondsLeft - penalty;
-    WoR.textContent = "Wrong :( The correct answer " + "questions[questionsIndex].answer";
-} else {
-    WoR.textContent= "Correct!";
-    score + 10;
-};
-}
-
-questionsIndex++;
-
-if (questionsIndex === questions.legnth) {
-    finishQuiz();
-}
-
-function finishQuiz();
-    clearInterval(timerInterval);
-
-    highscoreReveal.textContent = score;
+    questionsIndex.choiceList.forEach(function(choice, i) {
     
+        var choiceBtn = document.createElement("button");
+        choiceBtn.setAttribute("class", "choice");
+        choiceBtn.setAttribute("value", choice);
+
+        choiceBtn.textContent = i + choice;
+
+         // display on the page
+         choiceList.appendChild(choiceBtn);
+        // attach click event listener to each choice
+        choiceBtn.onclick = clickedQuestion;
+     });
+    } //curly bracket from for loop
+ 
+//now, I need to make a function for the clicked question to determine whether or not it is correct
+    var clickedQuestion = function clickedQuestion() {
+
+    if(clickedQuestion.value !== questions[questionsIndex].answer) {
+        //penalty suffered
+        secondsLeft = secondsLeft - penalty;
+        WoR.textContent = "Wrong :( The correct answer " + "questions[questionsIndex].answer";
+    } else {
+        WoR.textContent= "Correct! You know your stuff!";
+        score = score + 10;
+    };
+    };
+
+    //go to length of index until at the end
+    questionsIndex++;
+
+    if (questionsIndex === questions.length) {
+        finishQuiz();
+    };
+
+ };
+
+function finishQuiz() {
+    clearInterval(timerInterval);
+    scoreReveal.textContent = score;
+    scoreReveal.getElementId("hallOfFame").style.display="block";
+
+    var next = document.createElement('button');
+    next.onclick = saveHighscore();
+
+};
+
+var revealHallOfFame = document.querySelector("#hallOfFame");
+
+function saveHighscore() {
+    var submitBtn = document.querySelector("#submit");
+    submitBtn.onclick = localStorage.getItem();
+}
