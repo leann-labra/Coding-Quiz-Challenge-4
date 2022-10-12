8efbebb62a61d9706403ecc6ea7dd9781a2eb4cb
leann-labra <leann.labra@gmail.com>
2022-10-12 07:26:28 -0700

    addedmore to correct answer function, not seeming to work yet. I attempted to also compile the score board

--- assets/game.js
+++ assets/game.js
@@ -12,6 +12,9 @@ var timer = document.querySelector("#timer");
 var question = document.querySelector("#question");
 var choiceList = document.querySelector("#choices");
 var quizContent = document.querySelector("#quiz-content");
+var WoR = document.querySelector("#WoR");
+
+var highscoreReveal = document.querySelector("#highscore");
 
 //timer 
 startBtn.addEventListener("click", function timerCountdown() {
@@ -44,10 +47,10 @@ function showQuestions() {
    question.textContent = questionShown;
    choices.textContent = questionChoices;
 
-   console.log(questionShown);
-   console.log(questionChoices);
+//    console.log(questionShown);
+//    console.log(questionChoices);
 
-question.choices.forEach(function(choice, i) {
+    question.choices.forEach(function(choice, i) {
     // creates a new button for each choice
     var choiceBtn = document.createElement("button");
     choiceBtn.setAttribute("class", "choice");
@@ -64,5 +67,28 @@ question.choices.forEach(function(choice, i) {
    }
 } 
 
-//now, I need to make a function for the clicked question to determine whether or not it is correct
 
+//now, I need to make a function for the clicked question to determine whether or not it is correct
+var clickedQuestion = function clickedQuestion() {
+
+if(this.value !== questions[questionsIndex].answer) {
+    //penalty suffered
+    secondsLeft= secondsLeft - penalty;
+    WoR.textContent = "Wrong :( The correct answer " + "questions[questionsIndex].answer";
+} else {
+    WoR.textContent= "Correct!";
+    score + 10;
+};
+}
+
+questionsIndex++;
+
+if (questionsIndex === questions.legnth) {
+    finishQuiz();
+}
+
+function finishQuiz();
+    clearInterval(timerInterval);
+
+    highscoreReveal.textContent = score;
+    
